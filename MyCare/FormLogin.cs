ad6e58423543273febbed1984eb5153f8e92480f
ducthangwru <thangnd520@gmail.com>
2017-09-14 23:13:15 +0700

    Login ok ngon rồi :))) mật khẩu trong db là md5 nhé

--- MyCare/FormLogin.cs
+++ MyCare/FormLogin.cs
@@ -8,6 +8,8 @@ using DevExpress.ExpressApp.Win.Core;
 using DevExpress.ExpressApp.Win.Layout;
 using DevExpress.ExpressApp.Win.Templates.ActionContainers;
 using DevExpress.XtraEditors;
+using MyCare.MyCareDataAccess.DataAcess;
+using System.Windows.Forms;
 
 namespace MyCare
 {
@@ -20,8 +22,44 @@ namespace MyCare
 
         private void btnDangNhap_Click(object sender, System.EventArgs e)
         {
-            FormMain frmMain = new FormMain();
-            frmMain.ShowDialog();
+            DangNhap();
+        }
+
+        private void teUsername_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                DangNhap();
+            }
+        }
+
+        private void DangNhap()
+        {
+            if(string.IsNullOrEmpty(teUsername.Text) || string.IsNullOrEmpty(tePassword.Text))
+            {
+                XtraMessageBox.Show("Tên tài khoản hoặc mật khẩu đang trống! Vui lòng kiểm tra lại!", "Đăng nhập");
+            }
+            else
+            {
+                if (TaiKhoanDB.DangNhap(teUsername.Text, tePassword.Text))
+                {
+                    FormMain frmMain = new FormMain();
+                    frmMain.ShowDialog();
+                }
+                else
+                {
+                    XtraMessageBox.Show("Tên tài khoản hoặc mật khẩu không đúng!", "Đăng nhập thất bại");
+                }
+            }
+           
+        }
+
+        private void tePassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                DangNhap();
+            }
         }
     }
 }
--- MyCare/MyCareDataAccess/Utils/Utils.cs
+++ MyCare/MyCareDataAccess/Utils/Utils.cs
@@ -11,5 +11,19 @@ namespace MyCare.MyCareDataAccess.Utils
         public static Utils instance = new Utils();
         private Utils() {}
         public static SqlDataHelper db = new SqlDataHelper();
+
+        public static byte[] encryptData(string data)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5Hasher = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] hashedBytes;
+            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+            hashedBytes = md5Hasher.ComputeHash(encoder.GetBytes(data));
+            return hashedBytes;
+        }
+        public static string md5(string data)
+        {
+            return BitConverter.ToString(encryptData(data)).Replace("-", "").ToLower();
+            //return data;
+        }
     }
 }
